6c4a25e8535aa851d9a17803811df92d51517c8b
Sanaz <staheri14@ku.edu.tr>
2020-07-29 06:53:15 -0700

    Comments for the question description

--- src/main/M1-W2.go
+++ src/main/M1-W2.go
@@ -1,5 +1,9 @@
 package main
-
+/**
+Write a program which prompts the user to enter a floating point number and prints the integer which is
+a truncated version of the floating point number that was entered.
+Truncation is the process of removing the digits to the right of the decimal place.
+ */
 import (
 "fmt"
 "strconv"
