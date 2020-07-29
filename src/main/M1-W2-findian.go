cd4404bd81a8e9c8ba922097bfa3edf6b0e9e3fb
Sanaz <staheri14@ku.edu.tr>
2020-07-29 07:40:22 -0700

    Adds code to handle scan's error

--- src/main/M1-W2-findian.go
+++ src/main/M1-W2-findian.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 /*Write a program which prompts the user to enter a string.
 The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’.
 The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’,
@@ -13,12 +8,21 @@ The program should not be case-sensitive, so it does not matter if the character
 
 Examples: The program should print “Found!” for the following example entered strings, “ian”, “Ian”, “iuiygaygn”,
 “I d skd a efju N”. The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”.
- */
+*/
+
+import (
+	"fmt"
+	"strings"
+)
+
 
 func main(){
 	var in string
 	fmt.Println("Enter a string:")
-	fmt.Scan(&in)
+	_, err := fmt.Scan(&in)
+	if err != nil{
+		panic(err)
+	}
 	hasi:=strings.HasPrefix(in,"i")
 	hasn:=in[len(in)-1]=='n'
 	hasa:=strings.Contains(in,"a")
@@ -27,7 +31,5 @@ func main(){
 	}else{
 		fmt.Println("Not Found!")
 	}
-
-
 
 }
