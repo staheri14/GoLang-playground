e62efc5167aeb6481ddfcb27ec9bb5e54e5ff65d
Sanaz <staheri14@ku.edu.tr>
2020-07-29 06:53:44 -0700

    M1-W2-findian.go solution

--- src/main/M1-W2-findian.go
+++ src/main/M1-W2-findian.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*Write a program which prompts the user to enter a string.
+The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’.
+The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’,
+and contains the character ‘a’. The program should print “Not Found!” otherwise.
+The program should not be case-sensitive, so it does not matter if the characters are upper-case or lower-case.
+
+Examples: The program should print “Found!” for the following example entered strings, “ian”, “Ian”, “iuiygaygn”,
+“I d skd a efju N”. The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”.
+ */
+
+func main(){
+	var in string
+	fmt.Println("Enter a string:")
+	fmt.Scan(&in)
+	hasi:=strings.HasPrefix(in,"i")
+	hasn:=in[len(in)-1]=='n'
+	hasa:=strings.Contains(in,"a")
+	if(hasi && hasn && hasa){
+		fmt.Println("Found!")
+	}else{
+		fmt.Println("Not Found!")
+	}
+
+
+
+}
