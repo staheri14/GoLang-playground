4610bbe95c852bad3976f41012b7c6e4e60e04a1
Sanaz <staheri14@ku.edu.tr>
2020-08-13 09:58:56 -0700

    Integrates ioutility func calls into plusMinus.go

--- src/main/plusMinus.go
+++ src/main/plusMinus.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	_ "fmt"
 	_ "io"
+	"main/helper"
 	"os"
 	"strconv"
 	"strings"
 )
 
 //Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+//Input Format
+//
+//The first line contains an integer, , the size of the array.
+//The second line contains  space-separated integers
 
-// Complete the plusMinus function below.
+// the plusMinus function below.
 func plusMinus(arr []int32) {
 
 	var zcount, pcount, ncount float32
@@ -36,20 +41,23 @@ func plusMinus(arr []int32) {
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-	checkError(err)
+	// read the size of the array
+	nTemp, err := strconv.ParseInt(helper.ReadLine(reader), 10, 64)
+	helper.CheckError(err)
 	n := int32(nTemp)
 
-	arrTemp := strings.Split(readLine(reader), " ")
+	//read the array elements
+	arrTemp := strings.Split(helper.ReadLine(reader), " ")
 
 	var arr []int32
 
 	for i := 0; i < int(n); i++ {
 		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
-		checkError(err)
+		helper.CheckError(err)
 		arrItem := int32(arrItemTemp)
 		arr = append(arr, arrItem)
 	}
 
+	// prints the percentage of the positive, negative and zero values
 	plusMinus(arr)
 }
