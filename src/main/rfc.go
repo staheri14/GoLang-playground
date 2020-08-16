68479c36a06d6d01f5cf95c998c8f98d0b75dc9c
Sanaz <staheri14@ku.edu.tr>
2020-08-16 07:50:43 -0700

    Adds the RFC practice

---
+++ src/main/rfc.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"main/helper"
+	"net/http"
+)
+
+func main() {
+	resp , err :=http.Get("https://web3.bamboohr.com/jobs/")
+	helper.CheckError(err)
+
+	fmt.Println(resp.Status)
+
+}
