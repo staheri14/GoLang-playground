12fa932991323ec9ba10843b0cd05fda7a61d3e2
Sanaz <staheri14@ku.edu.tr>
2020-08-16 07:58:19 -0700

    Adds filesreadwrite..go containing practices of ioutil package

---
+++ src/main/filesreadwrite.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"main/helper"
+)
+
+func main(){
+	in,err:=ioutil.ReadFile("hello.txt")
+	helper.CheckError(err)
+	//in is a byte array
+	fmt.Println("The byte representation of the input file is: " ,in)
+	fmt.Println("The string representation of the input file is: " ,string(in))
+
+}
