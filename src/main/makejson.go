1224e01e638ae82de74d4c4caa275026a8f4b7b9
Sanaz <staheri14@ku.edu.tr>
2020-08-17 06:59:25 -0700

    Adds makejson.go

---
+++ src/main/makejson.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"main/helper"
+)
+
+//Write a program which prompts the user to first enter a name, and then enter an address.
+//Your program should create a map and add the name and address to the map using the keys “name” and “address”,
+//respectively.
+//Your program should use Marshal() to create a JSON object from the map,
+//and then your program should print the JSON object
+
+
+func main(){
+	var name, address string
+	fmt.Println("Enter a name: ")
+	fmt.Scan(&name)
+	fmt.Println("Enter an address: ")
+	fmt.Scan(&address)
+
+	//inserts the address to a map
+	 addressbook:= make(map[string]string,1)
+	 addressbook[name]=address
+
+	 //create the JSON object
+	 jobj,err:=json.Marshal(addressbook)
+	 helper.CheckError(err)
+
+	 fmt.Println("The JSON object is: ",string(jobj))
+
+	 //check the unmarshalled version
+	 add:=make(map[string]string)
+	 json.Unmarshal(jobj,&add)
+	 fmt.Println("After unmarshalling: the address of", name, " is ", add[name])
+}
+
+
