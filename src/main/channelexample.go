56617a1a8786d1bdd8ff378b515fb6d1890a96cb
Sanaz <staheri14@ku.edu.tr>
2020-08-19 08:50:14 -0700

    Adds the prod computation using Go channels into channelexample.go

--- src/main/channelexample.go
+++ src/main/channelexample.go
@@ -1,3 +1,18 @@
 package main
 
+import "fmt"
+
+func main(){
+	c:= make(chan int)
+	go prod(1,2,c)
+	go prod(3,4,c)
+	a:=<-c
+	b:=<-c
+	fmt.Println("The product of 1,2,3,4 is ",a*b)
+
+}
+
+func prod(a,b int,c chan int ){
+	c<-a*b
+}
 
