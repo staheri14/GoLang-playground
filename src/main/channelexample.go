3de1ea06ce00d20cb333dec9bc8f51e6034def8e
Sanaz <staheri14@ku.edu.tr>
2020-08-19 08:51:55 -0700

    Adds comments to the channelexample.go

--- src/main/channelexample.go
+++ src/main/channelexample.go
@@ -6,6 +6,7 @@ func main(){
 	c:= make(chan int)
 	go prod(1,2,c)
 	go prod(3,4,c)
+	//read the products from the channel
 	a:=<-c
 	b:=<-c
 	fmt.Println("The product of 1,2,3,4 is ",a*b)
@@ -13,6 +14,7 @@ func main(){
 }
 
 func prod(a,b int,c chan int ){
+	//computes a*b and sends it through the channel
 	c<-a*b
 }
 
