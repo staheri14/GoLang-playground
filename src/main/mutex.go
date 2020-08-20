2d54113c38784c564a84405a6551055e41efbd95
Sanaz <staheri14@ku.edu.tr>
2020-08-20 08:59:27 -0700

    Adds mutex.go
    This  uses sync.Mutex to fix the unexpected interleaving of the routines in goroutinesvariablesharing.go

---
+++ src/main/mutex.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//the global  variable shared between routines
+var j int=0
+var wgr sync.WaitGroup
+
+var mu sync.Mutex
+func incr(){
+	mu.Lock()
+	j=j+1
+	mu.Unlock()
+	wgr.Done()
+
+}
+
+
+func main(){
+	wgr.Add(2)
+	go incr()
+	go incr()
+	wgr.Wait()
+	fmt.Println(j)
+}
