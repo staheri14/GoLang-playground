e994f8cfbc5a7a8018e233cc907bcbceb011e8f7
Sanaz <staheri14@ku.edu.tr>
2020-08-20 08:57:52 -0700

    Adds the goroutinesvariablesharing.go
    Contains practices of sharing a global variable between two go routines using WaitGroup

---
+++ src/main/goroutinesvariablesharing.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//the global  variable shared between routines
+var i int=0
+var wg sync.WaitGroup
+
+func inc(){
+	i=i+1
+	wg.Done()
+}
+
+
+func main(){
+	wg.Add(2)
+	go inc()
+	go inc()
+	wg.Wait()
+	fmt.Println(i)
+}
