0df04765109c10f9284d124dc2f3fb64f46f25f5
Sanaz <staheri14@ku.edu.tr>
2021-06-29 07:58:27 -0700

    adds comments

--- Addison-Wesley-Book/CH1/fetch.go
+++ Addison-Wesley-Book/CH1/fetch.go
@@ -14,11 +14,14 @@ import "net/http"
 
 func main() {
 	url := os.Args[1]
+
+	// make the request
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error occured in http get %s", err.Error())
 		os.Exit(1)
 	}
+	// The Body is of type io.ReadCloser snd can be read by  ioutil.ReadAll
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "err occured while reading the response body %s", err.Error())
